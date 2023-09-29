da6f77643d931aad35b3d1f89dd18e87cdcb17fc
leonardo.pol <leonardo.pol@vodafone.com>
2023-09-29 19:13:01 +0200

    feat(scripts): add mongodb data seeding 🍃

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,5 @@
 echo "Starting Cleanup..."
+echo "Shutting Down Container and Volumes ⛔"
 docker-compose -f docker/docker-compose.yml down
 docker volume rm docker_quizz-data
-echo "Clean Completed!"
+echo "Cleanup Completed! ✅"
--- scripts/init-mongodb.js
+++ scripts/init-mongodb.js
@@ -1,7 +1,41 @@
 const database = "quizz";
-const collections = ["questions", "candidates"];
+const collections = ["quizzes", "questions", "transactions", "scores"];
 
 use(database);
-collections.forEach((value, index) => {
+
+collections.forEach((value) => {
   db.createCollection(value);
 });
+
+db.quizzes.insertMany([
+  {
+    name: "history quiz",
+    category: "history",
+    description: "this quiz is intended for kids",
+    difficulty: "easy",
+    tags: [
+      "history",
+      "easy",
+      "kids",
+      "colombo",
+      "french-revolution",
+      "italy-of-italy",
+    ],
+    createdAt: new Date(),
+  },
+  {
+    name: "math quiz",
+    category: "math",
+    description: "this quiz is intended for teenagers",
+    difficulty: "medium",
+    tags: [
+      "math",
+      "medium",
+      "teenagers",
+      "additions",
+      "fractions",
+      "equations",
+    ],
+    createdAt: new Date(),
+  },
+]);
